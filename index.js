9315460d71f7e1522345908681dd77bc0a188542
Eissa Purnell <eissamonet@gmail.com>
2023-08-13 16:54:40 -0500

    write file function

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 
 // importing shapes from shapes.js
 const { Circle, Triangle, Square} = require('./lib/shapes');
-const { type } = require('os');
 
 // Constructor class
 class svg{
@@ -50,7 +49,53 @@ const promptUser = () => {
             type: 'input',
             name: 'shape-color',
             message: 'Enter your desired shape color (OR a hexadecimal number)',
-        }
+        },
 
     ]);
 };
+
+function writeToFile(fileName, data) {
+    let svgString = '';
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">'
+    svgString += '<g>';
+    svgString += '${data.shape}';
+
+    let shapeChoice;
+    if (answers.shape === 'Triangle') {
+        shapeChoice = new Triangle();
+        svgString += '<polygon points="150, 18 244, 182 56, 182" fill="${data.shape-color}"/>';
+    } else if (data.shape ==='Square') {
+        shapeChoice = new Square();
+        svgString += '<rect x="73" y="40" width="160" height="160" fill="${data.shape-color}"/>';
+    } else {
+        shapeChoice = new Circle();
+        svgString += '<circle cx="150" cy="100" r="80" fill="${data.shape-color}"/>';
+    }
+
+    svgString += '<text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.text-color}">${data.text}</text>';
+    svgString += '<g>';
+    svgString += '</svg';
+
+};
+    // fs to generate svg file
+
+const writeFile = data => {
+    fs.writeFile(fileName, svgString, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('Generated logo.svg!')
+        }
+        
+    })
+};
+
+promptUser()
+.then((answers => {
+    if (answers.text.length > 3) {
+        console.log('Please enter no more than 3 characters');
+    } else {
+        writeToFile('logo.svg', answers);
+    }
+}));
