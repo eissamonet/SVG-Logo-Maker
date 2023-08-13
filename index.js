5fc6b04a5038b855252f123ac65123405a37a75f
Eissa Purnell <eissamonet@gmail.com>
2023-08-13 10:39:03 -0500

    added required packages

---
+++ index.js
@@ -0,0 +1,6 @@
+// packages for this application
+const inquirer = requirer('inquirer');
+const fs = require('fs');
+
+// importing shapes from shapes.js
+const { Circle, Triangle, Square} = require('./lib/shapes');
