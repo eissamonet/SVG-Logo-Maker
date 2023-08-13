5aa56bf0ac2f723a358e8e25df58be1e5e5690fb
Eissa Purnell <eissamonet@gmail.com>
2023-08-13 13:39:26 -0500

    added questions for users

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 // importing shapes from shapes.js
 const { Circle, Triangle, Square} = require('./lib/shapes');
+const { type } = require('os');
 
 // Constructor class
 class svg{
@@ -21,3 +22,35 @@ class svg{
         this.textElement = '<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>'
     }
 }
+
+// prompt user to answer questions
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to 3 characters',
+        },
+        {
+            type: 'input',
+            name: 'text-color',
+            message: 'Enter a color keyword (OR a hexadecimal number)',
+        },
+        {   type: 'list',
+            name: 'shape',
+            message: 'Choose your desired shape',
+            choices: [
+                'Circle',
+                'Triangle',
+                'Square',
+            ]
+        },
+        {
+            type: 'input',
+            name: 'shape-color',
+            message: 'Enter your desired shape color (OR a hexadecimal number)',
+        }
+
+    ]);
+};
