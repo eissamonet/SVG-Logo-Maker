38d439f6fd67e8aebc93ed5c9e1cfc1f33fe18a3
Eissa Purnell <eissamonet@gmail.com>
2023-08-13 12:50:13 -0500

    added constructor class

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ const fs = require('fs');
 
 // importing shapes from shapes.js
 const { Circle, Triangle, Square} = require('./lib/shapes');
+
+// Constructor class
+class svg{
+    constructor(){
+        this.textElement = ""
+        this.shapeElement = ""
+    }
+    render(){
+        return '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg'
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+    setTextElement(text, color){
+        this.textElement = '<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>'
+    }
+}
